0d8bc0afc880a3bc77cf4199e6359a3050eeca66
kylethanas <617078920@qq.com>
2019-03-12 17:20:58 +0800

    1

--- readbook/assets/js/34.f1f06cae.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{97:function(n,w,o){}}]);
---
+++ src/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'KyleThanas',
+    description: 'KyleThanas的分享',
+    plugins: [
+        [
+            'vuepress-plugin-yuque', {
+                repoUrl: 'https://www.yuque.com/kylethanas/share',
+                html: true,
+            }
+        ]
+    ]
+}
