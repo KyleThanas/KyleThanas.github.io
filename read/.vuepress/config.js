3a7a457e6eeaf28445d98685a735d25caf0ac2c1
kylethanas <617078920@qq.com>
2019-03-12 15:36:57 +0800

    1

---
+++ read/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Ant Design 1111',
+    description: '基于 umi 的 Ant Design 1111',
+    plugins: [
+        [
+            'vuepress-plugin-yuque', {
+                repoUrl: 'https://www.yuque.com/ant-design/course',
+            }
+        ]
+    ]
+}
