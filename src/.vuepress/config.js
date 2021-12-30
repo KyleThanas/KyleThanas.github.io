999665a3ab039dd6569e68a0be41063c1c00eb65
xiaozheng.zhang <xiaozheng.zhang@niu.com>
2021-12-30 18:35:51 +0800

    1

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,16 +1,22 @@
+const switchQIANDUAN = true
+const path = switchQIANDUAN ? 'share' : 'admin'
+const base = switchQIANDUAN ? '/kyle/' : '/kyle2/'
+const dest = switchQIANDUAN ? '/dist' : '/dist2'
+
 module.exports = {
   title: 'KyleThanas',
   description: 'KyleThanas的分享',
   head: [
-    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'referrer', content: 'never' }],
     ['meta', { name: 'og:type', content: 'webpage' }],
     ['meta', { name: 'og:url', content: 'https://KyleThanas.github.io' }],
     ['meta', { name: 'og:title', content: 'KyleThanas知识分享' }],
     ['meta', { name: 'og:description', content: 'KyleThanas的分享' }],
   ],
-  base: '/kyle/',
-  dest: '.vuepress/../../dist', //指定 vuepress build 的输出目录
+  base: base,
+  // base: '/kyle2/',
+  dest: `.vuepress/../../${dest}`, //指定 vuepress build 的输出目录
   themeConfig: {
     nav: [
       {
@@ -32,9 +38,10 @@ module.exports = {
       'vuepress-plugin-yuque',
       {
         // 爬坑之路
-        repoUrl: 'https://www.yuque.com/kylethanas/share',
+        // repoUrl: 'https://www.yuque.com/kylethanas/share',
         // 后端爬坑之路
         // repoUrl: 'https://www.yuque.com/kylethanas/admin',
+        repoUrl: `https://www.yuque.com/kylethanas/${path}`,
         authToken: 'u1XU3a9AAwSEDtNjHXOXZ5ry0xlXwbHlxL49zn9a',
         html: true,
         home: {
